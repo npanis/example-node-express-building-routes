6bcfa14dbe876091c05a7071d0019aee2a43e2c4
npanis <neriza.montes@gmail.com>
2024-07-29 22:48:35 +0900

    hope this works

--- src/app.js
+++ src/app.js
@@ -1,13 +1,52 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
-const sayHello = (req, res, next) => {
-  res.send("Hello!");
+const sayHello = (req, res) => {
+  console.log(req.query);
+  const name = req.query.name;
+  const content = name ? `Hello, ${name}!` : "Hello!";
+  res.send(content);
 };
 
-app.get("/hello", sayHello);
+const saySomething = (req, res) => {
+  const greeting = req.params.greeting;
+  const name = req.query.name;
+
+  const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}!`;
+  res.send(content);
+};
+
+const sayGoodbye = (req, res) => {
+  res.send("Sorry to see you go!");
+};
+
+app.get('/songs', (req, res) => {
+  const title = req.query.title;
+  res.send(title);
+});
+
+app.get('/states/:abbreviation', (req,res, next)=> {
+  const abbreviation = req.params.abbreviation;
+
+  if (abbreviation.length !==2 ){
+    next('State abbreviation is invalid.' );
+  } else {
+    res.send(`${abbreviation} is a nice state, I'd like to visit.`);
+  }
+});
+
+app.use((err, req,res, next) => {
+  console.error(err);
+  res.send(err);
+
+})
+
+app.get('/hello', sayHello);
+app.get("/say/goodbye", sayGoodbye);
+app.get("/say/:greeting", saySomething);
+
 
 module.exports = app;
